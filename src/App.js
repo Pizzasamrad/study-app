f6cf4f571d7007fce74411a9fe16f0e7b0c1f0f1
Pizzasamrad <Overload54101@gmail.com>
2025-07-23 20:12:34 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to My App for Kids!</h1>
+      <p>This is where your mission begins ✨</p>
+    </div>
+  );
+}
+
+export default App;
